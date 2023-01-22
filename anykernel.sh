a8164d59d66113cc511a747b30e118cdf4149c73
“SlackerState” <slackerstate@gmail.com>
2023-01-22 20:01:07 +0800

    changed for phoenix

--- anykernel.sh
+++ anykernel.sh
@@ -4,16 +4,16 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=Just add the KernelSU by SlackerState
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
+device.name1=phoenix
+device.name2=phoenixin
+device.name3=
+device.name4=
 device.name5=
 supported.versions=
 supported.patchlevels=
